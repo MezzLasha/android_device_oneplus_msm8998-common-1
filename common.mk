2e43dad06ee7baa3d4ca6a72668ac148bc231d32
Georg Veichtlbauer <georg@vware.at>
2021-09-29 08:22:29 +0200

    msm8998-common: Drop oppo/common dependency
    Change-Id: Icaf75039eb071f53f391983f6a8bcb9114b0dbd8

--- common.mk
+++ common.mk
@@ -490,9 +490,6 @@ PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl
 
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
