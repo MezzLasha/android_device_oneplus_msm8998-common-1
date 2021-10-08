72dd794116f89775fb092fd112cbe11908f5b12b
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-10-08 18:06:09 +0000

    msm8998-common: configure vndk versions
    Change-Id: Id5205f38df277b08aa7bd44f5d695b028d9c0089

--- common.mk
+++ common.mk
@@ -446,6 +446,10 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
 
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 29 30
+PRODUCT_EXTRA_VNDK_VERSIONS := $(PRODUCT_TARGET_VNDK_VERSION)
+
 # VR
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
